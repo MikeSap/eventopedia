34955cbd428d3053ce399a5baa40408d0aa3977b
tjbigelow <tjbigelow@att.net>
2020-10-21 15:47:20 -0500

    Wrapped everything in a container and centered login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,8 @@ class UsersController < ApplicationController
 
     def update
         @user = User.find(params[:id])
-        return  render :edit unless @user.update(user_params)
+        @permissions = @user.vendor.permission_classes
+        return render :edit unless @user.update(user_params)
         redirect_to user_path(@user)
     end
 
