f2015abc6066a83e2422327c990c6d1cb8ef730b
tjbigelow <tjbigelow@att.net>
2020-10-22 13:45:01 -0500

    Cleaned up datetime displays for shows

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,5 +6,12 @@ class Show < ApplicationRecord
     has_many :users, through: :technician_bookings
 
     validates :name, :venue, :client, :start, :end, presence: true
-    
+
+    def start_display
+        self.start.localtime.strftime("%b %-d %Y %l:%M %p")
+    end
+
+    def end_display
+        self.end.localtime.strftime("%b %-d %Y %l:%M %p")
+    end
 end
