7cd94ce7b805e7f0923406c1ab7389cd38f3c38b
mike <mikesapienzaaudio@gmail.com>
2020-10-22 19:42:43 -0500

    lx seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,37 +68,67 @@ cable_lengths.each do |length|
     Equipment.create(name: "#{length} SDI", manufacturer: "N/A", quantity: (rand(50) + 1), category: "Video", sub_category: "Cables", vendor:flatiron)
 end
 
-
+##audio seed
 speaker_manufacturers = %w(L-Acoustics Martin\ Audio D&B\ Audiotechnik Meyer\ Sound Void\ Acoustics Funktion-One JBL EAW)
 console_manufacturers = %w(Digico Avid Yamaha Soundcraft)
-cable_manufacturers = %w(Gepco Neutrik Mogami Whirlwind)
+cable_manufacturers = %w(Gepco Neutrik Mogami Whirlwind LEX)
 line_array = %w(K1 K2 Lyon Leo Leopard V-Dosc DV-DOSC V J KSL W Q Vertec VRX)
-subwoofer = %w(SB18 1100 KS28 VLFC 700-HP 500-HP BOOM/ BOOM 218 F-21 J-SUB V-SUB)
+subwoofer = %w(SB18 1100 KS28 VLFC 700-HP 500-HP 218 F-21 J-SUB V-SUB)
 console = %w(SD10 SD11 SD9 SD7 Profile SC48 VI-4 VI-6 )
 wedge = %w(Micro/ Wedge M4 M2 MJF-210 MJF-212)
 cable_types = %w(XLR NL4 NL8 Fiber\ Reel BNC XLR-5 L14-30 L21-30 Socapex)
-sub_categories = %w(Line\ Array Subwoofer Wedge Mixing\ Console)
+#sub_categories = %w(Line\ Array Subwoofer Wedge Mixing\ Console)
 cable = %w(5 10 25 50 100 150 250 328)
+
 10.times do
-    Equipment.create(name: line_array.sample, manufacturer: speaker_manufacturers.sample, quantity: (rand(50) + 1), category: 'Audio', sub_category: 'Line Array', vendor: flatiron)
+    Equipment.create(name: line_array.sample, manufacturer: speaker_manufacturers.sample, quantity: (rand(25) + 1), category: 'Audio', sub_category: 'Line Array', vendor: flatiron)
 end
 
 10.times do
-    Equipment.create(name: subwoofer.sample, manufacturer: speaker_manufacturers.sample, quantity: (rand(50) + 1), category: 'Audio', sub_category: 'Subwoofer', vendor: flatiron)
+    Equipment.create(name: subwoofer.sample, manufacturer: speaker_manufacturers.sample, quantity: (rand(25) + 1), category: 'Audio', sub_category: 'Subwoofer', vendor: flatiron)
 end
 
 10.times do
-    Equipment.create(name: console.sample, manufacturer: console_manufacturers.sample, quantity: (rand(50) + 1), category: 'Audio', sub_category: 'Mixing Console', vendor: flatiron)
+    Equipment.create(name: console.sample, manufacturer: console_manufacturers.sample, quantity: (rand(25) + 1), category: 'Audio', sub_category: 'Mixing Console', vendor: flatiron)
 end
 
 10.times do
-    Equipment.create(name: wedge.sample, manufacturer: speaker_manufacturers.sample, quantity: (rand(50) + 1), category: 'Audio', sub_category: 'Monitor Wedge', vendor: flatiron)
+    Equipment.create(name: wedge.sample, manufacturer: speaker_manufacturers.sample, quantity: (rand(25) + 1), category: 'Audio', sub_category: 'Monitor Wedge', vendor: flatiron)
 end
 
 25.times do
     Equipment.create(name: "#{cable.sample}' #{cable_types.sample}", manufacturer: cable_manufacturers.sample, quantity: (rand(50) + 1), category: 'Audio', sub_category: 'Cable', vendor: flatiron)
 end
 
+##lighting seed
+light_manufacturers = %w(Martin\ Lights ADJ Chauvet Elation Avo Antari Philips Clay\ Paky Robe Ayrton)
+lx_console_manufacturers = %w(MA Avolights Hog\ Systems)
+mover = %w(5r\ Beam 5r\ Spot Sharpy Profile Viper Magic\ Panel Magic\ Blade )
+strobe = %w(Atomic Protron Showline Solaris JDC1)
+lx_console = %w(grandMA grandMA2 grandMA3 Leprecon Full\ Boar\ 4 Hog\ 4 MagicQ Lil'\ Wing)
+wash = %w(Aura Quantum Showline Mac\ 700 Rouge\ R1 Sharpy\ Wash Viper\ Wash)
+lx_cable_types = %w(DMX Fiber\ Reel BNC L6-20 L21-30 Socapex)
+#sub_categories = %w(Moving\ Head Strobe Wash Console)
+
+15.times do
+    Equipment.create(name: mover.sample, manufacturer: light_manufacturers.sample, quantity: (rand(25) + 1), category: 'Lighting', sub_category: 'Mover', vendor: flatiron)
+end
+
+15.times do
+    Equipment.create(name: strobe.sample, manufacturer: light_manufacturers.sample, quantity: (rand(25) + 1), category: 'Lighting', sub_category: 'Strobe', vendor: flatiron)
+end
+
+10.times do
+    Equipment.create(name: lx_console.sample, manufacturer: lx_console_manufacturers.sample, quantity: (rand(25) + 1), category: 'Lighting', sub_category: 'Lighting Console', vendor: flatiron)
+end
+
+10.times do
+    Equipment.create(name: wash.sample, manufacturer: light_manufacturers.sample, quantity: (rand(25) + 1), category: 'Lighting', sub_category: 'Wash Light', vendor: flatiron)
+end
+
+25.times do
+    Equipment.create(name: "#{cable.sample}' #{lx_cable_types.sample}", manufacturer: cable_manufacturers.sample, quantity: (rand(50) + 1), category: 'Lighting', sub_category: 'Cable', vendor: flatiron)
+end
 
 
 
